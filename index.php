e8d39633430a39af0433072f730d9c68c2a7cb29
Daniel Pillwein <9132@htl.rennweg.at>
2023-02-22 13:45:37 +0100

    backend improved

--- index.php
+++ index.php
@@ -3,17 +3,11 @@ require './Hotel_Seeder.php';
 
 $index_template = file_get_contents('./templates/index.html');
 $hotels_HTML = '';
-$data = file_get_contents('./hotels.json');
-$data = json_decode($data, true);
-$data = $data['hotels'];
-
-
-$hotel_object = new Hotel('DANI','BESCHREIBUNG','LINK');
 
 $seeder = new Hotel_Seeder();
-$data = $seeder->getHotels();
+$hotels = $seeder->getHotels();
 
-foreach($data as $hotel){
+foreach($hotels as $hotel){
     $hotel_template = file_get_contents('./templates/hotel.html');
     $hotel_template = str_replace('###NAME###',$hotel->getName(),$hotel_template);
     $hotel_template = str_replace('###DESCRIPTION###',$hotel->getDescription(),$hotel_template);
