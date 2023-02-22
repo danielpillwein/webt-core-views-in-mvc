c108588bb6b2a0f31cc194819a12239de0b026f9
Gabriel Forstner <8134@htl.rennweg.at>
2023-02-22 12:56:19 +0100

    us2 finished

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-echo file_get_contents('./templates/index.html');
+
+$hotel1 = isset($_GET['hotel1']) ? $_GET['hotel1'] : 'Italy-Hotel';
+$hotel2 = isset($_GET['hotel2']) ? $_GET['hotel2'] : 'Austria-Hotel';
+$hotel3 = isset($_GET['hotel3']) ? $_GET['hotel3'] : 'USA-Hotel';
+
+$template = file_get_contents('./templates/index.html');
+$template = str_replace('###HOTEL1###', $hotel1, $template);
+$template = str_replace('###HOTEL2###', $hotel2, $template);
+$template = str_replace('###HOTEL3###', $hotel3, $template);
+
+echo $template;
