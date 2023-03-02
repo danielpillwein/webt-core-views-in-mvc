136492a57599e289214e37df7031d9eec789c545
Daniel Pillwein <9132@htl.rennweg.at>
2023-03-02 17:34:55 +0100

    done done

--- index.php
+++ index.php
@@ -4,33 +4,30 @@ require './vendor/autoload.php';
 use Dgl\WebtCoreViewsInMvc\Hotel;
 use Dgl\WebtCoreViewsInMvc\HotelSeeder;
 
-$index_template = file_get_contents('./templates/index.html');
-$hotels_HTML = '';
-
-// $seeder = new HotelSeeder();
 $hotels = HotelSeeder::getHotels();
-/* 
-foreach($hotels as $hotel){
-    @var $hotel Hotel
-    $hotel_template = file_get_contents('./templates/hotel.html');
-    $hotel_template = str_replace('###NAME###',$hotel->getName(),$hotel_template);
-    $hotel_template = str_replace('###DESCRIPTION###',$hotel->getDescription(),$hotel_template);
-    $hotel_template = str_replace('###IMAGELINK###',$hotel->getImageLink(),$hotel_template);
-    $hotels_HTML .= $hotel_template;
-}
-*/
 $loader = new \Twig\Loader\FilesystemLoader('./templates');
 $twig = new \Twig\Environment($loader);
-$template = $twig->load('index.html');
+$template = $twig->load('one-column.html');
 
 echo $template->render([
-    'authors' => ['Dani','Luki','Gabriel'],
-    'navigation' => [[
-        'link' => 'https://www.booking.com/index.de.html',
+    'authors' => ['Dani','|','Luki','|','Gabriel'],
+    'navigation' => [
+        [
+        'link' => 'https://www.booking.com',
         'name' => 'Booking.com'
-    ],[
-        'link' => 'https://www.motel-one.com/de/',
-        'name' => 'MOTEL ONE'],
+        ],
+        [
+        'link' => 'https://www.expedia.com',
+        'name' => 'Expedia'
+        ],
+        [
+        'link' => 'https://www.hotels.com',
+        'name' => 'Hotels.com'
+        ],
+        [
+        'link' => 'https://www.agoda.com',
+        'name' => 'Agoda'
+        ]
     ],
     'hotels' => $hotels]
 );
