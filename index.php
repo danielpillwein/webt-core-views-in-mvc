261f02ce5d9c1b4ba274971a7ee6f179f50f2e01
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-03-22 12:30:48 +0100

    fluid with partial working

--- index.php
+++ index.php
@@ -1,55 +1,28 @@
 <?php
 require './vendor/autoload.php';
 
-/*
- * This file belongs to the package "TYPO3 Fluid".
- * See LICENSE.txt that was shipped with this package.
- */
+use Dgl\WebtCoreViewsInMvc\Hotel;
+use Dgl\WebtCoreViewsInMvc\HotelSeeder;
 
-/**
- * EXAMPLE: Variables usage
- *
- * This example shows you how to use variables in
- * the Fluid template language and illustrates
- * how dynamic variable access works.
- */
+$hotels[] = new Hotel("Wynn", "asdfsa", "asdfsa");
+$hotels[] = new Hotel("Excalibur", "dsafa", "sdfasdf");
 
-// set up paths object with arrays of paths with files
-// $paths = new \TYPO3Fluid\Fluid\View\TemplatePaths();
-// $paths->setTemplateRootPaths(['./Resources/Private/Templates']);
-// $paths->setLayoutRootPaths(['./Resources/Private/Layouts']);
-// $paths->setPartialRootPaths(['./Resources/Private/Partials']);
-// pass the constructed TemplatePaths instance to the View
+// Initializing the View: rendering in Fluid takes place through a View instance
+// which contains a RenderingContext that in turn contains things like definitions
+// of template paths, instances of variable containers and similar.
 $view = new \TYPO3Fluid\Fluid\View\TemplateView();
-
-$view->assignMultiple([
-    // Casting types
-    'types' => [
-        'csv' => 'one,two',
-        'aStringWithNumbers' => '132 a string',
-        'anArray' => ['one', 'two'],
-        'typeNameInteger' => 'integer'
-    ],
-    'foobar' => 'string foo',
-    // The variables we will use as dynamic part names:
-    // Strings we will be accessing dynamically:
-    // Arrays we will be accessing dynamically:
-    'array' => [
-        'fixed' => 'Fixed key in $array[fixed]',
-        // A numerically indexed array which we will access directly.
-        'numeric' => [
-            'foo',
-            'bar'
-        ],
-    ],
-    '123numericprefix' => 'Numeric prefixed variable',
-    // A variable whose value refers to another variable name
-    'dynamicVariableName' => 'foobar'
-]);
-
+// TemplatePaths object: a subclass can be used if custom resolving is wanted.
 $paths = $view->getTemplatePaths();
-$paths->setTemplatePathAndFilename(__DIR__ . '/Resources/Private/Templates/Variables.html');
-
+// Assigning the template path and filename to be rendered. Doing this overrides
+// resolving normally done by the TemplatePaths and directly renders this file.
+$paths->setTemplatePathAndFilename('resources/templates/main.html');
+$paths->setPartialRootPaths(['resources/partials/']);
+// In this example we assign all our variables in one array. Alternative is
+// to repeatedly call $view->assign('name', 'value').
+$view->assignMultiple(
+    ['hotels' => $hotels]
+);
+// Rendering the View: plain old rendering of single file, no bells and whistles.
 $output = $view->render();
 
 echo $output;
